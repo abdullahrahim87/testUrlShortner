0fc924bf49bf55da769fa352ec1148fd99900b68
abdullahrahim87 <abdullahrahim87@gmail.com>
2017-08-15 00:16:43 +0500

    added some unit tests for urls class

--- src/test/groovy/urlshortner/UrlsSpec.groovy
+++ src/test/groovy/urlshortner/UrlsSpec.groovy
@@ -2,21 +2,34 @@ package urlshortner
 
 import grails.test.mixin.TestFor
 import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(Urls)
 class UrlsSpec extends Specification {
-
-    def setup() {
+    @Unroll('validate on a person with age #personAge should have returned #shouldBeValid')
+    void "test url"() {
+        expect:
+         new Urls(url: url, shortUrl:"http://google.com", userId:  12).validate() == shouldBeValid
+        where:
+        url | shouldBeValid
+        ""       | false
+        "asdf"        | false
+        "http://google.com"         | true
     }
 
-    def cleanup() {
+    @Unroll('validate on a person with age #personAge should have returned #shouldBeValid')
+    void "test shorturl"() {
+        expect:
+        new Urls(url: "http://google.com", shortUrl:url, userId:  12).validate() == shouldBeValid
+        where:
+        url | shouldBeValid
+        ""       | false
+        "asdf"        | false
+        "http://google.com"         | true
     }
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
-    }
+
 }
